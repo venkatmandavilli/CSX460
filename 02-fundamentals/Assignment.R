d98d1311c00592a6968433ff9d6369bb14058492
Venkatesh M <venkatmandavilli@gmail.com>
2016-10-11 19:53:58 -0700

    Assignment 2

---
+++ 02-fundamentals/Assignment.R
@@ -0,0 +1,57 @@
+getwd()
+setwd("C:/Users/Venkatesh/Documents/Machine Learning/tmp/CSX460/02-fundamentals/data")
+
+#1
+airports <- read.csv("airports.csv")
+flights <- read.csv("flights.csv")
+planes <- read.csv("planes.csv")
+weather <- read.csv("weather.csv")
+
+#2
+hist(flights$arr_delay,main="Histogram for Air Passengers", 
+     xlab="Arrival Delay", 
+     border="blue", 
+     col="green")
+
+hist(flights$dep_delay,main="Histogram for Air Passengers", 
+     xlab="Departure Delay", 
+     border="blue", 
+     col="green")
+
+#3
+plot(flights$arr_delay, flights$dep_delay,main="Scatterplot for Air Passengers", 
+     xlab="Arrival Delay", ylab="Departure Delay", pch=20, col="green")
+
+#4
+install.packages("data.table")
+library("data.table")
+
+FLIGHTS <- data.table(flights$origin,flights$carrier)
+setnames(FLIGHTS,"V1","Origin")
+setnames(FLIGHTS,"V2","Carrier")
+FLIGHTS
+
+
+#5
+airportTable <- data.table(airports)
+flightTable <- data.table(flights)
+planeTable <- data.table(planes)
+weatherTable <- data.table(weather)
+
+setkey(airportTable,faa)
+setkey(flightTable,origin)
+airportFlightTable <- airportTable[flightTable,nomatch=0]
+setkey(airportFlightTable,tailnum)
+setkey(planeTable,tailnum)
+airportFlightPlaneTable <- airportFlightTable[planeTable,nomatch=0]
+setkey(airportFlightPlaneTable,faa)
+setkey(weatherTable,origin)
+Result <- airportFlightPlaneTable[weatherTable, allow.cartesian = TRUE]
+Result
+  
+  
+  
+  
+  
+  
+
